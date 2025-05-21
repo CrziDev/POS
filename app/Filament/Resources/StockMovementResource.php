29062e87fc0102f13e3bdf0e7c724cd910d7b72c
CraziDev <jmichaelbughao@gmail.com>
2025-05-21 23:13:58 +0800

    wip

--- app/Filament/Resources/StockMovementResource.php
+++ app/Filament/Resources/StockMovementResource.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\StockMovementResource\Pages;
+use App\Models\Employee;
 use App\Models\StockMovement;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -11,39 +12,41 @@ use Filament\Tables\Table;
 
 class StockMovementResource extends Resource
 {
-    protected static ?string $model = StockMovement::class;
+    protected static ?string $model = Employee::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-arrows-right-left';
     protected static ?string $navigationLabel = 'Stock Movement';
     protected static ?string $navigationGroup = 'Inventory';
+    protected static ?string $label = 'Still in Development';
+
     protected static ?int $navigationSort = 4;
 
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-            ]);
-    }
+    // public static function form(Form $form): Form
+    // {
+    //     return $form
+    //         ->schema([
+    //         ]);
+    // }
 
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                //
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
-    }
+    // public static function table(Table $table): Table
+    // {
+    //     return $table
+    //         ->columns([
+    //             //
+    //         ])
+    //         ->filters([
+    //             //
+    //         ])
+    //         ->actions([
+    //             Tables\Actions\EditAction::make(),
+    //         ])
+    //         ->bulkActions([
+    //             Tables\Actions\BulkActionGroup::make([
+    //                 Tables\Actions\DeleteBulkAction::make(),
+    //             ]),
+    //         ]);
+    // }
 
     public static function getRelations(): array
     {
@@ -56,8 +59,8 @@ class StockMovementResource extends Resource
     {
         return [
             'index' => Pages\ListStockMovements::route('/'),
-            'create' => Pages\CreateStockMovement::route('/create'),
-            'edit' => Pages\EditStockMovement::route('/{record}/edit'),
+            // 'create' => Pages\CreateStockMovement::route('/create'),
+            // 'edit' => Pages\EditStockMovement::route('/{record}/edit'),
         ];
     }
 }
